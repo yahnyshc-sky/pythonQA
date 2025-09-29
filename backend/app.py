1cee5538dd4bb08e620b269eae9f215b83f01adc
Jibril Abdi <jibrilabdi01@gmail>
2025-09-29 10:15:41 +0100

    implemented backend

---
+++ backend/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import random
+app = Flask(__name__)
+choices = [
+    "r",
+    "p",
+    "s",
+]
+
+def choose_winner(user_choice):
+    comp_choice = random.choice(choices)
+    if comp_choice == user_choice:
+        return {'comp_choice': comp_choice, 'user_choice':user_choice, 'result': "tie"}
+    elif comp_choice == "r" and user_choice == "s" or \
+    comp_choice == "p" and user_choice == "r" or \
+    comp_choice == "s" and user_choice == "p":
+        return {'comp_choice': comp_choice, 'user_choice': user_choice, 'result': "comp"}
+    else:
+        return {'comp_choice': comp_choice, 'user_choice': user_choice, 'result': "user"}
+
+@app.route('/', methods=['POST'])
+def index():
+    user_choice = request.json.get('choice').lower()
+    return jsonify(choose_winner(user_choice))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
